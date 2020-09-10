801ad00456f5f22aa65cdad5a206b8e4a86c58c1
Fatlum <fgjinofci@gmail.com>
2020-09-10 20:30:04 +0200

    working on layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    <h1 class="text-grey-900">MiMe</h1>
+  <div class="text-center py-4">
+    <h1 class="text-grey-900 text-2xl py-4">MiMe</h1>
     <form @submit.prevent="getGifs">
-      <input v-model="searchGif" class="border" type="text" />
-      <button class="border" @keyup.enter="getGifs">Search</button>
+      <input v-model="searchGif" placeholder="Search meme word ;)" class="border border-gray-500 shadow-lg outline-none rounded-full mx-2 px-3 py-1" type="text" />
+      <button class="border border-gray-500 rounded-full shadow-lg px-4 py-1 outline-none text-gray-500" @keyup.enter="getGifs">Search</button>
     </form>
 
-    <div class="">
-      <img v-for="gif in gifs" :src="gif" :key="gif.id" />
+
+    <div class="flex flex-col pt-10">
+        <div class="w-full lg:w-1/2 mx-auto" v-for="gif in gifs" :key="gif.id">
+            <img class="p-2 rounded-lg shadow-2xl" :src="gif" />
+        </div>
     </div>
   </div>
 </template>
@@ -18,8 +21,8 @@ import axios from "axios";
 export default {
   data() {
     return {
-      searchGif: "",
-      gifs: [],
+        searchGif: "",
+        gifs: [],
     };
   },
 
@@ -30,13 +33,11 @@ export default {
       let limit = 5;
 
       let url = `${searchEndPoint}&api_key=${apiKey}&q=${this.searchGif}&limit=${limit}`;
-
       axios
         .get(url)
         .then((response) => {
           this.buildGifs(response);
         })
-
         .catch((error) => console.log(error));
     },
 
