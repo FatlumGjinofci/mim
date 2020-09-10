fc6554737203fd5d77ac6997d18d3a513576d456
Fatlum <fgjinofci@gmail.com>
2020-09-10 20:48:10 +0200

    trying to add clipboard on img click

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,13 +3,13 @@
     <h1 class="text-grey-900 text-2xl py-4">MiMe</h1>
     <form @submit.prevent="getGifs">
       <input v-model="searchGif" placeholder="Search meme word ;)" class="border border-gray-500 shadow-lg outline-none rounded-full mx-2 px-3 py-1" type="text" />
-      <button class="border border-gray-500 rounded-full shadow-lg px-4 py-1 outline-none text-gray-500" @keyup.enter="getGifs">Search</button>
+      <button class="border border-gray-500 rounded-full shadow-lg px-4 py-1 outline-none text-gray-500 bg-gray-300" @keyup.enter="getGifs">Search</button>
     </form>
 
 
     <div class="flex flex-col pt-10">
         <div class="w-full lg:w-1/2 mx-auto" v-for="gif in gifs" :key="gif.id">
-            <img class="p-2 rounded-lg shadow-2xl" :src="gif" />
+            <img  class="p-2 rounded-lg shadow-2xl" :src="gif" />
         </div>
     </div>
   </div>
@@ -27,27 +27,29 @@ export default {
   },
 
   methods: {
-    getGifs() {
-      let apiKey = "dc6zaTOxFJmzC";
-      let searchEndPoint = "https://api.giphy.com/v1/gifs/search?";
-      let limit = 5;
+      getGifs() {
+          let apiKey = "dc6zaTOxFJmzC";
+          let searchEndPoint = "https://api.giphy.com/v1/gifs/search?";
+          let limit = 5;
 
-      let url = `${searchEndPoint}&api_key=${apiKey}&q=${this.searchGif}&limit=${limit}`;
-      axios
-        .get(url)
-        .then((response) => {
-          this.buildGifs(response);
-        })
-        .catch((error) => console.log(error));
-    },
-
-    buildGifs(json) {
-      this.gifs = json.data.data
-        .map((gif) => gif.id)
-        .map((gifId) => {
-          return `https://media.giphy.com/media/${gifId}/giphy.gif`;
-        });
-    },
+          let url = `${searchEndPoint}&api_key=${apiKey}&q=${this.searchGif}&limit=${limit}`;
+          axios
+            .get(url)
+            .then((response) => {
+              this.buildGifs(response);
+            })
+            .catch((error) => console.log(error));
+        },
+      buildGifs(json) {
+          this.gifs = json.data.data
+            .map((gif) => gif.id)
+            .map((gifId) => {
+              return `https://media.giphy.com/media/${gifId}/giphy.gif`;
+            });
+        },
+      copyLink() {
+          console.log(this.$clipboard('test img'));
+      }
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
+import Clipboard from 'v-clipboard';
 
+
+Vue.use(Clipboard);
 Vue.config.productionTip = false;
 
 new Vue({
