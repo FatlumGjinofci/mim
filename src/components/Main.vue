370f870cb413a7048bef6ac6968c4bc877efb2c4
unknown <fatlum@urbanway.net>
2020-09-11 16:22:34 +0200

    working on random gifs

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="text-center py-4">
-    <h1 class="text-grey-900 text-2xl py-4">MiMe</h1>
+    <h1 class="text-grey-900 text-2xl py-4">m3m3</h1>
     <form @submit.prevent="getGifs">
       <input v-model="searchGif" placeholder="Search meme word ;)" class="border border-gray-500 shadow-lg outline-none rounded-full mx-2 px-3 py-1" type="text" />
-      <button class="border border-gray-500 rounded-full shadow-lg px-4 py-1 outline-none text-gray-500 bg-gray-300" @keyup.enter="getGifs">Search</button>
+      <button class="border border-gray-500 rounded-full shadow-lg px-4 py-1 outline-none text-gray-500 bg-gray-300 hover:bg-gray-200 hover:text-gray-700" @click="getGifs">Search</button>
     </form>
 
 
@@ -11,6 +11,11 @@
         <div class="w-full lg:w-1/2 mx-auto" v-for="gif in gifs" :key="gif.id" @click="copyLink()" >
             <img class="p-2 rounded-lg shadow-2xl" :src="gif"/>
         </div>
+    </div>
+    <div class="flex flex-col pt-10">
+      <div class="w-full lg:w-1/2 mx-auto" v-for="gif in gifs" :key="gif.id" @click="copyLink()" >
+        <img class="p-2 rounded-lg shadow-2xl" :src="gif"/>
+      </div>
     </div>
   </div>
 </template>
@@ -27,13 +32,13 @@ export default {
     };
   },
 
+  created() {
+    // this.getRandomGifs()
+  },
+
   methods: {
       getGifs() {
-          let apiKey = "dc6zaTOxFJmzC";
-          let searchEndPoint = "https://api.giphy.com/v1/gifs/search?";
-          let limit = 5;
-
-          let url = `${searchEndPoint}&api_key=${apiKey}&q=${this.searchGif}&limit=${limit}`;
+          let url = `${process.env.VUE_APP_API}/search?&api_key=${process.env.VUE_APP_GIPHY_KEY}&q=${this.searchGif}&limit=${process.env.VUE_APP_LIMIT}`;
           axios
             .get(url)
             .then((response) => {
@@ -42,15 +47,23 @@ export default {
             .catch((error) => console.log(error));
         },
       buildGifs(json) {
-        this.gifs = json.data.data;
-          this.gifs = json.data.data
+        this.gifs = json.data.data
             .map((gif) => gif.id)
             .map((gifId) => {
               return `https://media.giphy.com/media/${gifId}/giphy.gif`;
             });
         },
-      copyLink() {
-        console.log(this.gifs)
+      getRandomGifs() {
+        // gifs/random?api_key=Ou5M8sZ88o3dTxjP60xJkN84Ig4QJyKb&tag=&rating=g
+        let url = `${process.env.VUE_APP_API}/trending?api_key=${process.env.VUE_APP_GIPHY_KEY}&limit=${process.env.VUE_APP_LIMIT}&rating=g`;
+
+        axios.get(url).then(response => {
+          // console.log(response.data.data.images)
+          response.data.data.map(gif => {
+            this.gifs = gif.images.original.url
+            console.log(this.gifs);
+          })
+        })
       }
   },
 };
