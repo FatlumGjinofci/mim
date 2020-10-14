6073858ec9d6a0e7124c02cadbeee178c8ee0923
unknown <fatlum@urbanway.net>
2020-10-14 16:12:07 +0200

    added hooper slider

---
+++ src/components/Giphy/Trending.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <p>Trending Gifs</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            trendingGifs: null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
